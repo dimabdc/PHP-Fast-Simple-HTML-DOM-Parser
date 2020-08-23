4b7e79c1c7dd4f3118ba8151cc61bb5fca3874f7
dimabdc <support@titor.ru>
2020-08-24 02:46:03 +0400

    Improve selector converter for text and comment elements

--- lib/SelectorConverter.php
+++ lib/SelectorConverter.php
@@ -20,16 +20,6 @@ class SelectorConverter
             return self::$compiled[$selector];
         }
 
-        // Select DOMText
-        if ($selector === 'text') {
-            return '//text()';
-        }
-
-        // Select DOMComment
-        if ($selector === 'comment') {
-            return '//comment()';
-        }
-
         if (!class_exists('Symfony\\Component\\CssSelector\\CssSelectorConverter')) {
             throw new \RuntimeException('Unable to filter with a CSS selector as the Symfony CssSelector 2.8+ is not installed (you can use filterXPath instead).');
         }
@@ -37,6 +27,11 @@ class SelectorConverter
         $converter = new CssSelectorConverter(true);
 
         $xPathQuery = $converter->toXPath($selector);
+
+        $xPathQuery = preg_replace_callback('/(:(\*\/)?|\/)(text|comment)(?=[\s]*[^.\w\[\]#]|$)/', static function (array $match) {
+            return $match[0] . '()';
+        }, $xPathQuery);
+
         self::$compiled[$selector] = $xPathQuery;
 
         return $xPathQuery;
